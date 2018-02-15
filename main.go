3a600f6ba761caf176c7b8fddbe4764aaa055549
Sergio Zarth Jr <zarthjr@gmail.com>
2018-02-15 12:06:44 -0200

    Refactor para novo uso e mudança da estrutura

--- main.go
+++ main.go
@@ -6,19 +6,24 @@ import (
 	"strings"
 )
 
+// CsvIn Estrutura de recebimento do arquivo
 type CsvIn struct {
-	IdEntrevista string
-	IdEntrevistador string
-	IMEI string
-	Questao string
-	Resposta string
+	IDEntrevista string
+	IDQuestao    string
+	IMEI         string
+	Questao      string
+	Resposta     string
 }
+
+var (
+	origin      = "*"
+	destination = "*"
+)
 
 func main() {
 
-	filename := "*"
+	f, err := os.Open(origin)
 
-	f, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -28,7 +33,7 @@ func main() {
 	reader := csv.NewReader(f)
 	reader.Comma = ';'
 
-	lines, _ := reader.ReadAll()
+	lines, err := reader.ReadAll()
 
 	if err != nil {
 		panic(err)
@@ -37,99 +42,38 @@ func main() {
 	var dataIn []CsvIn
 
 	for _, line := range lines {
+
 		line := CsvIn{
-			IdEntrevista:    line[0],
-			IdEntrevistador: line[1],
-			IMEI:            line[2],
-			Questao:         line[3],
-			Resposta:        line[4],
+			IDEntrevista: line[0],
+			IMEI:         line[1],
+			IDQuestao:    line[2],
+			Questao:      line[3],
+			Resposta:     line[4],
 		}
+
 		dataIn = append(dataIn, line)
 	}
 
 	var dataOut [][]string
 
 	for e := range dataIn {
-		var resposta = strings.Split(dataIn[e].Resposta, "&")
-
-		resposta1 := ""
-		resposta2 := ""
-		resposta3 := ""
-		resposta4 := ""
-		resposta5 := ""
-		resposta6 := ""
-		resposta7 := ""
-		resposta8 := ""
-		resposta9 := ""
-		resposta10 := ""
-		resposta11:= ""
-
-		if len(resposta) > 0{
-			resposta1 = resposta[0]
-		}
-
-		if len(resposta) > 1{
-			resposta2 = resposta[1]
-		}
-
-		if len(resposta) > 2{
-			resposta3 = resposta[2]
-		}
-
-		if len(resposta) > 3{
-			resposta4 = resposta[3]
-		}
-
-		if len(resposta) > 4{
-			resposta5 = resposta[4]
-		}
-
-		if len(resposta) > 5{
-			resposta6 = resposta[5]
-		}
-
-		if len(resposta) > 6{
-			resposta7 = resposta[6]
-		}
-
-		if len(resposta) > 7{
-			resposta8 = resposta[7]
-		}
-
-		if len(resposta) > 8{
-			resposta9 = resposta[8]
-		}
-
-		if len(resposta) > 9{
-			resposta10 = resposta[9]
-		}
-
-		if len(resposta) > 10 {
-			resposta11 = resposta[10]
-		}
 
 		outLine := []string{
-			dataIn[e].IdEntrevista,
-			dataIn[e].IdEntrevistador,
+			dataIn[e].IDEntrevista,
+			dataIn[e].IDQuestao,
 			dataIn[e].IMEI,
-			dataIn[e].Questao,
-			resposta1,
-			resposta2,
-			resposta3,
-			resposta4,
-			resposta5,
-			resposta6,
-			resposta7,
-			resposta8,
-			resposta9,
-			resposta10,
-			resposta11,
+			dataIn[e].Questao}
+
+		var resposta = strings.Split(dataIn[e].Resposta, "&")
+
+		for x := range resposta {
+			outLine = append(outLine, resposta[x])
 		}
 
 		dataOut = append(dataOut, outLine)
 	}
 
-	fileOut, err := os.Create("/home/zarthjr/Downloads/exportacao_entrevistas_corrigido_pos_processamento_2.csv")
+	fileOut, err := os.Create(destination)
 	defer fileOut.Close()
 	writer := csv.NewWriter(fileOut)
 	writer.Comma = ';'
@@ -140,18 +84,3 @@ func main() {
 	}
 
 }
-	//Código usado para descobrir o número de colunas necessárias
-	/*
-	r := regexp.MustCompile("&")
-	var control = 0
-	var str string
-	for e := range data {
-		var count = len(r.FindAllStringIndex(data[e].Resposta, -1))
-		if count > control{
-			control = count
-			str = data[e].Resposta
-		}
-	}
-
-	fmt.Print(control, " , ", str)
-	*/
